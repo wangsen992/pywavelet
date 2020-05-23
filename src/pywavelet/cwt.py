4cbb2e70f6a67a86ab4d7df1a4bfaa5c8521a1e3
Sen Wang <wangsen992@gmail.com>
2020-05-23 10:53:53 +0800

    update covariance calc with xarray

--- src/pywavelet/cwt.py
+++ src/pywavelet/cwt.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import xarray as xr
 import scipy.fftpack as fft
 
 def compute_scale_range(s0, dj, j1):
@@ -26,10 +27,15 @@ def reconstruct_signal(W_n, wavelet, scales, dj, dt):
     scaling_factor = dj * dt**0.5 / (wavelet.C_delta * wavelet.psi_0_0)
     return scaling_factor * (np.real(W_n) / scales[:,np.newaxis]**0.5).sum(axis=0)
 
-def compute_covariance(W_n1, W_n2, wavelet, scales, dj, dt):
+def compute_covariance(WW_n, wavelet, scales, dj, dt):
+    """Implemetation based on xarray input instead of numpy array
+
+    Input: 
+        WW_n: real values of product of wavelet coefficients. 
+    """
+
     scaling_factor = dj * dt / wavelet.C_delta
-    return (np.real(W_n1 * np.conjugate(W_n2)) / \
-            scales[:,np.newaxis]).sum(axis=0) * scaling_factor
+    return (WW_n / WW_n.scale).sum(dim='scale') * scaling_factor
 
 def compute_energy_density(W_n, scales):
     return np.abs(W_n * np.conjugate(W_n)) / scales[:,np.newaxis]**0.5
